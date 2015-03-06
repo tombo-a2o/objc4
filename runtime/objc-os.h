3dabb1267e47467ead579d9d18a89937fb31dd4c
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-03-06 18:34:32 +0900

    Implement malloc_zone_memalign

--- runtime/objc-os.h
+++ runtime/objc-os.h
@@ -946,7 +946,7 @@ static __inline void *malloc_zone_realloc(malloc_zone_t z, void *p, size_t size)
 static __inline void malloc_zone_free(malloc_zone_t z, void *p) { free(p); }
 static __inline malloc_zone_t malloc_zone_from_ptr(const void *p) { return (malloc_zone_t)-1; }
 static __inline size_t malloc_size(const void *p) { return malloc_usable_size((void*)p); /* fixme invalid pointer check? */ }
-static __inline void *malloc_zone_memalign(malloc_zone_t z, size_t size, size_t count) { return 0; }
+static __inline void *malloc_zone_memalign(malloc_zone_t z, size_t size, size_t count) { return memalign(size, count); }
 
 // OSAtomic
 
