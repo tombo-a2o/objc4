00199021994f498df0fb28c7c50ed298f554cdfe
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-04-28 23:42:25 +0900

    malloc.h impl is moved to emscripten

--- runtime/objc-os.h
+++ runtime/objc-os.h
@@ -946,16 +946,7 @@ extern int secure_open(const char *filename, int flags, uid_t euid);
 
 #   define SUPPORT_DIRECT_THREAD_KEYS 0
 
-typedef void * malloc_zone_t;
-
-static __inline malloc_zone_t malloc_default_zone(void) { return (malloc_zone_t)-1; }
-static __inline void *malloc_zone_malloc(malloc_zone_t z, size_t size) { return malloc(size); }
-static __inline void *malloc_zone_calloc(malloc_zone_t z, size_t size, size_t count) { return calloc(count, size); }
-static __inline void *malloc_zone_realloc(malloc_zone_t z, void *p, size_t size) { return realloc(p, size); }
-static __inline void malloc_zone_free(malloc_zone_t z, void *p) { free(p); }
-static __inline malloc_zone_t malloc_zone_from_ptr(const void *p) { return (malloc_zone_t)-1; }
-static __inline size_t malloc_size(const void *p) { return malloc_usable_size((void*)p); /* fixme invalid pointer check? */ }
-static __inline void *malloc_zone_memalign(malloc_zone_t z, size_t size, size_t count) { return memalign(size, count); }
+#include <malloc/malloc.h>
 
 // OSAtomic
 
